92878f696c7cea4c3b38b1ee28f877e0611470cb
Robin  Linacre <robinlinacre@hotmail.com>
2015-10-15 12:22:37 +0100

    tested solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+##Returns a list of functions the cache the inverse of a matrix in the closure  
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inverse <- NULL  #Initial value of the inverse, before it is stored
+  
+  #This function assigns a matrix into the closure, in preparation for computing the inverse
+  set <- function(y) {
+    
+    #The <<- operator gives r access to variables outside the scope of the function
+    x <<-y  
+
+    #If we have a new data, we need to delete any previous inverses which have been calculated
+    inverse <<- NULL 
+  }
+  
+  get <- function() x
+  
+  #set a value for the inverse into the closure 
+  setinverse <- function(inv) inverse <<- inv
+  
+  #get the cached version of inverse from the closure
+  getinverse <- function() inverse
+  
+  #return a list of the functions we'e just specified
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
-
+##Takes a cachematrix object and returns an inverse.
+#Computes inverse if not already computed, otherwise returns result from cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    
+    #If cached value exists, do not perform invese calculate
+    if(!is.null(inverse)){
+      message("getting inverse from cache")
+      return(inverse)
+    }
+    
+    #get the raw matrix from the cachematrix object
+    data <- x$get()
+    
+    #compute the inverse
+    inv <- solve(data,...)
+    
+    #assign it to the cache
+    x$setinverse(inv)
+    
+    #return the inverse 
+    inv
+  
 }
+
+#test it works
+c_inv <- makeCacheMatrix(matrix(runif(10000),100,100))
+
+cacheSolve(c_inv)
+cacheSolve(c_inv)
